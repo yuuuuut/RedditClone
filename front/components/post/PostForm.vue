d427de2ff04f587d602919e8eb7f70e5fe05f152
yuuuuut <yutodazepa@icloud.com>
2021-10-15 15:34:17 +0900

    refs #7 add change target select box

--- front/components/post/PostForm.vue
+++ front/components/post/PostForm.vue
@@ -1,141 +1,187 @@
 <template>
-  <v-card class="card">
-    <NuxtLink to="/u/username/submit">Test</NuxtLink>
-    <NuxtLink to="/submit">Submit</NuxtLink>
-    <v-tabs
-      v-model="tab"
-      background-color="white"
-      grow
-    >
-      <v-tabs-slider></v-tabs-slider>
-      <v-tab href="#tab-1">
-        Post
-      </v-tab>
-      <v-tab href="#tab-2">
-        Image
-      </v-tab>
-      <v-tab href="#tab-3">
-        Link
-      </v-tab>
-    </v-tabs>
-    <v-tabs-items v-model="tab" class="pa-3">
-      <v-tab-item
-        v-for="(tab, i) in tabs"
-        :key="i"
-        :value="'tab-' + String((Number(i) + 1))"
-        :transition="false"
-      >
-        <v-card flat>
-          <v-text-field
-            v-model="post.title"
-            placeholder="Title"
-            hide-details="auto"
-            outlined
-            class="mb-2"
-          />
-          <div v-if="tab === 'POST'">
-            <v-textarea
-              v-model="post.text"
-              placeholder="Text(optional)"
-              hide-details="auto"
-              outlined
-            />
+  <div class="form">
+    <div class="d-flex justify-space-between">
+      <h3 class="mb-3">Create a post</h3>
+      <div>DRAFTS</div>
+    </div>
+    <v-divider class="divider" />
+    <div class="target-select">
+      <v-menu offset-y>
+        <template #activator="{ on, attrs }">
+          <div v-bind="attrs" class="target-select__content" v-on="on">
+            <v-avatar v-if="selectTargetIcon.isAvatar" class="mr-3" size="25">
+              <v-img :src="selectTargetIcon.url" />
+            </v-avatar>
+            <div v-else class="not-avatar mr-3" />
+            <div>{{ selectTargetText }}</div>
           </div>
-          <div v-if="tab === 'IMAGE'">
-            <div class="image-area">
-              <div v-if="!postImage.url" class="image-area__inner">
-                  <v-file-input
-                    id="imageInput"
-                    accept="image/*"
-                    label="File input"
-                    style="display: none;"
-                    @change="imageUpload"
-                  ></v-file-input>
-                  <v-btn color="primary" @click="btnclick">Upload Image</v-btn>
-              </div>
-              <div v-else class="upload-image__area">
-                <img :src="postImage.url" class="image" @mouseover="imageMouseOver" @mouseleave="imageMouseLeave" />
-                <v-btn
-                  v-show="isImageHover"
-                  class="delete-icon mx-2"
-                  fab
-                  small
-                  dark
-                  color="black"
-                  @click="deleteImage"
-                  @mouseover="imageMouseOver"
-                >
-                  <v-icon>
-                    mdi-delete
-                  </v-icon>
-                </v-btn>
-              </div>
+        </template>
+        <div>
+          <div class="select-menu__user">
+            <div class="select-menu__item-title">your profile</div>
+            <div class="select-menu__item" @click="clickSelectUserItem">
+              <v-avatar class="mr-3" size="40" tile><v-img :src="currentUser.image" /></v-avatar>
+              <div>u/{{ currentUser.name }}</div>
             </div>
           </div>
-          <div v-if="tab === 'LINK'">
+        </div>
+      </v-menu>
+
+      <!-- <div class="target-select__content" @click="clickSelectTarget">
+        <v-avatar v-if="selectTargetIcon.isAvatar" class="mr-3" size="25">
+          <v-img :src="selectTargetIcon.url" />
+        </v-avatar>
+        <div v-else class="not-avatar mr-3" />
+        <div>{{ selectTargetText }}</div>
+      </div> -->
+                <!-- <div v-if="isSelectMenu" class="select-menu">
+            <div class="select-menu__user">
+              <div class="select-menu__item-title">your profile</div>
+              <div class="select-menu__item" @click="clickSelectUserItem">
+                <v-avatar class="mr-3" size="40" tile><v-img :src="currentUser.image" /></v-avatar>
+                <div>u/{{ currentUser.name }}</div>
+              </div>
+            </div>
+          </div> -->
+    </div>
+    <v-card>
+      <v-tabs
+        v-model="tab"
+        background-color="white"
+        grow
+      >
+        <v-tabs-slider></v-tabs-slider>
+        <v-tab href="#tab-1">
+          Post
+        </v-tab>
+        <v-tab href="#tab-2">
+          Image
+        </v-tab>
+        <v-tab href="#tab-3">
+          Link
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab" class="pa-3">
+        <v-tab-item
+          v-for="(tab, i) in tabs"
+          :key="i"
+          :value="'tab-' + String((Number(i) + 1))"
+          :transition="false"
+        >
+          <v-card flat>
             <v-text-field
-              v-model="post.url"
-              placeholder="Url"
+              v-model="post.title"
+              placeholder="Title"
               hide-details="auto"
-              height="50"
               outlined
+              class="mb-2"
             />
-          </div>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-    <div class="d-flex pa-3">
-      <v-chip
-        :outlined="!post.spoiler"
-        :color="post.spoiler ? 'black' : ''"
-        :text-color="post.spoiler ? 'white': ''"
-        class="mr-2"
-        @click="post.spoiler = !post.spoiler"
-      >
-        <v-icon v-if="!post.spoiler" left>
-          mdi-plus
-        </v-icon>
-        <v-icon v-else left>
-          mdi-check
-        </v-icon>
-        spoiler
-      </v-chip>
-      <v-chip
-        :outlined="!post.nsfw"
-        :color="post.nsfw ? 'black' : ''"
-        :text-color="post.nsfw ? 'white': ''"
-        @click="post.nsfw = !post.nsfw"
-      >
-        <v-icon v-if="!post.nsfw" left>
-          mdi-plus
-        </v-icon>
-        <v-icon v-else left>
-          mdi-check
-        </v-icon>
-        nsfw
-      </v-chip>
-    </div>
-    <v-divider />
-    <div class="d-flex justify-end pa-3">
-      <v-chip color="primary" class="mr-3">
-        SAVE DRAFT
-      </v-chip>
-      <v-chip color="black" text-color="white" :disabled="!post.title.length" @click="createPost">
-        POST
-      </v-chip>
-    </div>
-  </v-card>
+            <div v-if="tab === 'POST'">
+              <v-textarea
+                v-model="post.text"
+                placeholder="Text(optional)"
+                hide-details="auto"
+                outlined
+              />
+            </div>
+            <div v-if="tab === 'IMAGE'">
+              <div class="image-area">
+                <div v-if="!postImage.url" class="image-area__inner">
+                    <v-file-input
+                      id="imageInput"
+                      accept="image/*"
+                      label="File input"
+                      style="display: none;"
+                      @change="imageUpload"
+                    ></v-file-input>
+                    <v-btn color="primary" @click="btnclick">Upload Image</v-btn>
+                </div>
+                <div v-else class="upload-image__area">
+                  <img :src="postImage.url" class="image" @mouseover="imageMouseOver" @mouseleave="imageMouseLeave" />
+                  <v-btn
+                    v-show="isImageHover"
+                    class="delete-icon mx-2"
+                    fab
+                    small
+                    dark
+                    color="black"
+                    @click="deleteImage"
+                    @mouseover="imageMouseOver"
+                  >
+                    <v-icon>
+                      mdi-delete
+                    </v-icon>
+                  </v-btn>
+                </div>
+              </div>
+            </div>
+            <div v-if="tab === 'LINK'">
+              <v-text-field
+                v-model="post.url"
+                placeholder="Url"
+                hide-details="auto"
+                height="50"
+                outlined
+              />
+            </div>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+      <div class="d-flex pa-3">
+        <v-chip
+          :outlined="!post.spoiler"
+          :color="post.spoiler ? 'black' : ''"
+          :text-color="post.spoiler ? 'white': ''"
+          class="mr-2"
+          @click="post.spoiler = !post.spoiler"
+        >
+          <v-icon v-if="!post.spoiler" left>
+            mdi-plus
+          </v-icon>
+          <v-icon v-else left>
+            mdi-check
+          </v-icon>
+          spoiler
+        </v-chip>
+        <v-chip
+          :outlined="!post.nsfw"
+          :color="post.nsfw ? 'black' : ''"
+          :text-color="post.nsfw ? 'white': ''"
+          @click="post.nsfw = !post.nsfw"
+        >
+          <v-icon v-if="!post.nsfw" left>
+            mdi-plus
+          </v-icon>
+          <v-icon v-else left>
+            mdi-check
+          </v-icon>
+          nsfw
+        </v-chip>
+      </div>
+      <v-divider />
+      <div class="d-flex justify-end pa-3">
+        <v-chip color="primary" class="mr-3">
+          SAVE DRAFT
+        </v-chip>
+        <v-chip color="black" text-color="white" :disabled="!post.title.length" @click="createPost">
+          POST
+        </v-chip>
+      </div>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeUnmount, onMounted, ref, useRoute } from "@nuxtjs/composition-api"
+import { computed, defineComponent, onBeforeUnmount, onMounted, ref, useRoute, useRouter } from "@nuxtjs/composition-api"
 import { deleteObject, getDownloadURL, ref as r, uploadBytesResumable } from 'firebase/storage'
 import { storage } from "~/plugins/firebase"
+import { userStore } from "~/store"
 import { $axios } from "~/utils/api"
 
 export default defineComponent({
   setup() {
     const route = useRoute()
+    const router = useRouter()
 
     const tab = ref(null)
     const tabs = ref(['POST', 'IMAGE', 'LINK'])
@@ -151,6 +197,22 @@ export default defineComponent({
     const postImage = ref({
       uid: '',
       url: ''
+    })
+    const isSelectMenu = ref(false)
+
+    const selectTargetIcon = computed(() => {
+      const name = route.value.name
+      if (name === 'u-name-submit') return { isAvatar: true, url: currentUser.value.image }
+      return { isAvatar: false, url: '' }
+    })
+    const selectTargetText = computed(() => {
+      const name = route.value.name
+      if (name === 'u-name-submit') return currentUser.value.name
+      return 'Choose a community'
+    })
+
+    const currentUser = computed(() => {
+      return userStore.currentUser
     })
 
     onMounted(() => {
@@ -174,6 +236,16 @@ export default defineComponent({
         localStorage.setItem('post-image', JSON.stringify(postImage.value))
       }
     })
+
+    const clickSelectTarget = () => {
+      isSelectMenu.value = !isSelectMenu.value
+    }
+
+    const clickSelectUserItem = () => {
+      isSelectMenu.value = false
+      router.push(`/u/${currentUser.value.name}/submit`)
+      selectTargetText.value = 'u/' + currentUser.value.name
+    }
 
     const btnclick = () => {
       const elm = document.getElementById('imageInput') as HTMLElement
@@ -222,20 +294,76 @@ export default defineComponent({
       post,
       postImage,
       isImageHover,
+      isSelectMenu,
+      selectTargetText,
+      selectTargetIcon,
+      currentUser,
+      clickSelectTarget,
+      clickSelectUserItem,
       imageMouseOver,
       imageMouseLeave,
       btnclick,
       createPost,
       imageUpload,
-      deleteImage
+      deleteImage,
     }
   }
 })
 </script>
 
 <style lang="scss" scoped>
-.card {
+.v-menu__content {
+  background-color: #fff;
+  box-shadow: initial;
+  border: 1px solid #dae0e6;
+  max-height: 300px;
+}
+
+.form {
   max-width: 90%;
+  margin-top: 20px;
+
+  .divider {
+    margin-bottom: 20px;
+  }
+
+  .target-select {
+    margin-bottom: 10px;
+  }
+
+  .target-select__content {
+    position: relative;
+    max-width: 300px;
+    height: 40px;
+    background-color: white;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+  }
+
+  .not-avatar {
+    width: 22px;
+    height: 22px;
+    border-radius: 50%;
+    border: 1px dashed gray;
+  }
+}
+
+.select-menu__user {
+  border-bottom: 1px solid #dae0e6;
+  padding: 15px;
+}
+
+.select-menu__item-title {
+  font-size: 14px;
+  color: gray;
+  margin-bottom: 10px;
+}
+
+.select-menu__item {
+  display: flex;
+  align-items: center;
 }
 
 .image-area {
